916e3dc09131ee70a4e7f9cace32604e91ae5c0a
stiti <stiti0987@gmail.com>
2026-09-04 09:57:40 +0530

    style: apply standard Prettier indent formatting to MovieList

--- starter/frontend/src/components/MovieList.js
+++ starter/frontend/src/components/MovieList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+﻿import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 
@@ -6,15 +6,23 @@ function MovieList({ onMovieClick }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    axios.get(`http://adc443dfb195746a69128bd7c2142491-1230811994.us-east-1.elb.amazonaws.com/movies`).then((response) => {
-      setMovies(response.data.movies);
-    });
+    axios
+      .get(
+        'http://adc443dfb195746a69128bd7c2142491-1230811994.us-east-1.elb.amazonaws.com/movies'
+      )
+      .then((response) => {
+        setMovies(response.data.movies);
+      });
   }, []);
 
   return (
     <ul>
       {movies.map((movie) => (
-        <li className="movieItem" key={movie.id} onClick={() => onMovieClick(movie)}>
+        <li
+          className="movieItem"
+          key={movie.id}
+          onClick={() => onMovieClick(movie)}
+        >
           {movie.title}
         </li>
       ))}
